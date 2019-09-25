0b30b36aaedbc18c713363c187aa54e21d519e26
akhil dhiman <dhimanakhil95@gmail.com>
2019-09-25 18:32:25 +0530

    add controlled vs uncontrolled form component

---
+++ handle-input/handle_input/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import ControlledForm from './ControlledForm';
+import UncontrolledForm from './UncontrolledForm';
+
+function App() {
+  return (
+    <div>
+      <ControlledForm />
+      <UncontrolledForm />
+    </div>
+  )
+}
+
+export default App;
